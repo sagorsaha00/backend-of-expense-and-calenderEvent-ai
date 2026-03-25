f2952de4b6444bc2d41fa3e62cc8fe516d183240
Sagor saha <117963424+sagorsaha00@users.noreply.github.com>
2026-03-25 20:28:49 +0600

    g

--- src/router/token.ts
+++ src/router/token.ts
@@ -14,6 +14,6 @@ const tokencontroller = new tokenController(tokenService)
 
 
 
-router.post('/refreshToken', verifyAccessToken, (req, res) => tokencontroller.refreshToken(req, res))
+router.post('/refreshToken',  (req, res) => tokencontroller.refreshToken(req, res))
 
 export default router
